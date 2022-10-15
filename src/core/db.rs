2206469ab04e974daa009164cd999ac92dea106e
Raymond <raymond.peng07@gmail.com>
2022-10-15 19:12:57 +0000

    db init ok

--- src/core/db.rs
+++ src/core/db.rs
@@ -1,22 +1,82 @@
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 struct DatabaseMetadata{
-
+    name: String,
+    version: u32,
+    open: bool,
+    collections: Vec<String>
 }
 
-impl DatabaseMetadata {
-    
+impl Default for DatabaseMetadata {
+    fn default() -> DatabaseMetadata {
+        return DatabaseMetadata {
+            name: "default database".to_string(),
+            version: 1,
+            open: true,
+            collections: vec![] // empty
+        }
+    }
 }
 
-struct Database {
+pub struct Database {
     pub path: PathBuf,
+    meta: DatabaseMetadata
 }
 
 impl Database {
     pub fn new(path_str: String) -> Database{
-        return Database {
-            path: Path::new(&path_str).to_owned()
+        Database {
+            path: Path::new(&path_str).to_owned(),
+            meta: DatabaseMetadata::default()
+        }
+    }
+
+    // todo: store this somewhere so we don't keep recreating when we need it
+    // very microoptimization
+    pub fn get_meta_path(&self) -> PathBuf {
+        self.path.join("meta.json")
+    }
+
+    pub fn create_if_nonexist(&self) -> std::io::Result<()>{
+        // println!("creating {:?}",self.path.as_path().to_str());
+        fs::create_dir_all(self.path.as_path())?;
+        Ok(())
+    }
+
+    // TODO: rename meta_contents to a better name 
+    // TODO: passthrough errors?
+
+    pub fn save_meta(&self) -> std::io::Result<()>{
+        let mut file = File::create(self.get_meta_path().as_path()).expect("Meta file open fail. ");
+        let meta_contents = serde_json::to_string(&self.meta).expect("Serializing meta state failed. ");
+        file.write_all(meta_contents.as_bytes()).expect("Could not write to metadata file. ");
+        Ok(())
+    }
+
+    pub fn load_meta(&mut self) -> std::io::Result<()>{
+        let meta_contents = fs::read_to_string(self.get_meta_path().as_os_str())?;
+        self.meta = serde_json::from_str(&meta_contents)?;
+        Ok(())
+    }
+
+    // picked this name cause serenity used it
+    // if someone has a better name like init
+    // then sure I might change it
+
+    pub fn start(&mut self){
+        // let manifest_path = self.get_meta_path();
+        self.create_if_nonexist().expect("Database directory creation fail. ");
+        match self.load_meta() {
+            Err(err) => {
+                self.save_meta().expect("Initial save of metadata failed. ");
+                self.load_meta().expect("Initial reload of metadata failed. ");
+            },
+            Ok(_) => return, 
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tokio::join;
 
+use crate::core::db::Database;
 use crate::core::service::Service;
 use crate::core::test;
 use crate::bot::discord::DiscordBot;
@@ -18,7 +19,11 @@ async fn main() {
     // Test our services    
     test();
     // Database
-    
+    // TODO: Fix mess of a line
+    let mut db = Database::new(env::current_dir().expect("Could not get current directory").join("database").to_str().unwrap().to_owned());
+    db.start();
+    db.save_meta().expect("Save fail");
+
     // Create our services
     let mut bot = DiscordBot::new(env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set")).await;
     // Lmao so we just uh init all here
